adcb4bd5e75faa4a04cfdda3728c8f7a9f9dd297
branGitFox <vixfgit@gmail.com>
2023-11-05 05:12:52 +0100

    couleur pour le mode sombre fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 const toggleBtn = document.querySelector('.nav-toggle-icon')
 const hideMenu = document.querySelector('.hide-menu')
 const container = document.querySelector('.news')
+const switchContainer = document.querySelector('.switch')
+
 toggleBtn.addEventListener('click', () => {
     hideMenu.classList.toggle('show-menu')
     hideMenu.classList.toggle('hide-menu')
 })
 
-//th memmode sombre et mode clair
+//theme mode sombre et mode clair
+
+const switchCircle = document.querySelector('.switch-circle')
+const nav = document.querySelector('nav')
+const footer = document.querySelector('footer')
+const navLinks = document.querySelector('.nav-links')
+const containerBig = document.querySelector('.container')
+switchContainer.addEventListener('click', () => {
+    switchCircle.classList.toggle('leftRight')
+    switchCircle.classList.toggle('dark')
+    switchContainer.classList.toggle('switchDark')
+    nav.classList.toggle('nav-dark')
+    footer.classList.toggle('footer-dark')
+    navLinks.classList.toggle('nav-links-dark')
+    containerBig.classList.toggle('container-dark')
+})
 
