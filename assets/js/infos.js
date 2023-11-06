76c63235c4937f2fce84e10d4324ab73242e52ca
branGitFox <vixfgit@gmail.com>
2023-11-06 10:20:20 +0100

    ajout des icons dans les informations

---
+++ assets/js/infos.js
@@ -0,0 +1,12 @@
+let themeCode = localStorage.getItem('themeCode')
+const header = document.querySelector('header')
+const body = document.querySelector('body')
+if(themeCode % 2 != 0){
+    header.classList.add('header-dark')
+    body.style.backgroundColor = 'rgba(0, 0, 0, 0.89'
+    body.style.color = 'white'
+}else {
+    header.classList.remove('header-dark')
+    body.style.backgroundColor = 'white'
+    body.style.color = 'black'
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,54 @@
-const toggleBtn = document.querySelector('.nav-toggle-icon')
-const hideMenu = document.querySelector('.hide-menu')
-const container = document.querySelector('.news')
-const switchContainer = document.querySelector('.switch')
+const toggleBtn = document.querySelector(".nav-toggle-icon");
+const hideMenu = document.querySelector(".hide-menu");
+const container = document.querySelector(".news");
+const switchContainer = document.querySelector(".switch");
 
-toggleBtn.addEventListener('click', () => {
-    hideMenu.classList.toggle('show-menu')
-    hideMenu.classList.toggle('hide-menu')
-})
+toggleBtn.addEventListener("click", () => {
+  hideMenu.classList.toggle("show-menu");
+  hideMenu.classList.toggle("hide-menu");
+});
 
 //theme mode sombre et mode clair
-const switchCircle = document.querySelector('.switch-circle')
-const nav = document.querySelector('nav')
-const footer = document.querySelector('footer')
-const navLinks = document.querySelector('.nav-links')
-const containerBig = document.querySelector('.container')
-const hideMenuDark = document.querySelector('.hide-menu')
-switchContainer.addEventListener('click', () => {
-    switchCircle.classList.toggle('leftRight')
-    switchCircle.classList.toggle('dark')
-    switchContainer.classList.toggle('switchDark')
-    nav.classList.toggle('nav-dark')
-    footer.classList.toggle('footer-dark')
-    navLinks.classList.toggle('nav-links-dark')
-    containerBig.classList.toggle('container-dark')
-    hideMenuDark.classList.toggle('hide-menu-dark')
-    hideMenuDark.classList.toggle('show-menu-dark')
-})
+const switchCircle = document.querySelector(".switch-circle");
+const nav = document.querySelector("nav");
+const footer = document.querySelector("footer");
+const navLinks = document.querySelector(".nav-links");
+const containerBig = document.querySelector(".container");
+const hideMenuDark = document.querySelector(".hide-menu");
+switchContainer.addEventListener("click", () => {
+  let themeCode = localStorage.getItem("themeCode");
+  localStorage.setItem("themeCode", ++themeCode);
+  switchCircle.classList.toggle("leftRight");
+  switchCircle.classList.toggle("dark");
+  switchContainer.classList.toggle("switchDark");
+  nav.classList.toggle("nav-dark");
+  footer.classList.toggle("footer-dark");
+  navLinks.classList.toggle("nav-links-dark");
+  containerBig.classList.toggle("container-dark");
+  hideMenuDark.classList.toggle("hide-menu-dark");
+  hideMenuDark.classList.toggle("show-menu-dark");
+});
 
+// mode sombre automatique
+let themeCode = localStorage.getItem("themeCode");
+if (themeCode % 2 != 0) {
+  switchCircle.classList.add("leftRight");
+  switchCircle.classList.add("dark");
+  switchContainer.classList.add("switchDark");
+  nav.classList.add("nav-dark");
+  footer.classList.add("footer-dark");
+  navLinks.classList.add("nav-links-dark");
+  containerBig.classList.add("container-dark");
+  hideMenuDark.classList.add("hide-menu-dark");
+  hideMenuDark.classList.add("show-menu-dark");
+} else {
+  switchCircle.classList.remove("leftRight");
+  switchCircle.classList.remove("dark");
+  switchContainer.classList.remove("switchDark");
+  nav.classList.remove("nav-dark");
+  footer.classList.remove("footer-dark");
+  navLinks.classList.remove("nav-links-dark");
+  containerBig.classList.remove("container-dark");
+  hideMenuDark.classList.remove("hide-menu-dark");
+  hideMenuDark.classList.remove("show-menu-dark");
+}
