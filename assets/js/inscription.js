8d840a1149ab80f5ad78e954fa2681f622379527
branGitFox <vixfgit@gmail.com>
2023-11-13 09:10:49 +0100

    creation des fichier connexion.(css,js,html)

--- assets/js/inscription.js
+++ assets/js/inscription.js
@@ -13,3 +13,19 @@ closeEye.addEventListener('click', () => {
     openEye.style.display = 'block'
     input.type = 'password'
 })
+
+// mode sombre dynamic
+
+const body = document.querySelector('body')
+let themeCode = localStorage.getItem('themeCode')
+if(themeCode % 2 != 0){
+    closeEye.style.color = 'black'
+    openEye.style.color = 'black'
+    body.style.backgroundColor = 'rgba(0, 0, 0, 0.89'
+    body.style.color = 'white'
+}else {
+    body.style.backgroundColor = 'white'
+    body.style.color = 'black'
+    closeEye.style.color = 'black'
+    openEye.style.color = 'black'
+}
