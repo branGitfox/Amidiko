2952d19c65ce6987e3bcd4a3d3e99e5859254e7f
branGitFox <vixfgit@gmail.com>
2023-11-13 09:20:07 +0100

    page de connexion fini avec la fonction des yeux et theme sombre aussi

---
+++ assets/js/connexion.js
@@ -0,0 +1,32 @@
+const closeEye = document.querySelector('.close')
+const openEye = document.querySelector('.open')
+const input = document.querySelector('#password')
+
+
+openEye.addEventListener('click', () => {
+    openEye.style.display = 'none'
+    closeEye.style.display = 'block'
+    input.type = 'text'
+})
+
+closeEye.addEventListener('click', () => {
+    closeEye.style.display = 'none'
+    openEye.style.display = 'block'
+    input.type = 'password'
+})
+
+// mode sombre dynamic
+
+const body = document.querySelector('body')
+let themeCode = localStorage.getItem('themeCode')
+if(themeCode % 2 != 0){
+    closeEye.style.color = 'black'
+    openEye.style.color = 'black'
+    body.style.backgroundColor = 'rgba(0, 0, 0, 0.89'
+    body.style.color = 'white'
+}else {
+    body.style.backgroundColor = 'white'
+    body.style.color = 'black'
+    closeEye.style.color = 'black'
+    openEye.style.color = 'black'
+}
