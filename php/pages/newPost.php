d7cecc5ed080767241f68ea094eed024955b2e87
branGitFox <vixfgit@gmail.com>
2023-11-22 18:16:22 +0100

    lien de retour sur la page

--- php/pages/newPost.php
+++ php/pages/newPost.php
@@ -22,7 +22,7 @@ $userImage = $user->getUseImageprofil($_SESSION['user']['user_id']);
 <body>
     <div class="container">
         <nav>
-            <div class="return-icon"><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+            <div class="return-icon"><a href="logEd"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
               </svg></a></div>
             <h2><?= $category->checkNewFormPost() ?></h2>
