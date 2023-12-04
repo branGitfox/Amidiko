887a3aca39024e71383eea9e35e6efa955badece
branGitFox <vixfgit@gmail.com>
2023-12-04 18:11:36 +0100

    essayant de creer un design pour afficher le pub d'un utlisateur par son ID

--- php/pages/showPub.php
+++ php/pages/showPub.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 require '../class/UserActions.php';
@@ -8,3 +8,26 @@ require '../class/PubActions.php';
 $pub = new PubActions();
 echo $pub->showPubById($pub->getPubId())['user_name'];
 echo $pub->showPubById($pub->getPubId())['pub_desc'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/voirannonce.css">
+    <title>Amidiko | Job</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="info-container">
+            <p class="infos">Par
+                <?= $pub->showPubById($pub->getPubId())['user_name'] ?>
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
